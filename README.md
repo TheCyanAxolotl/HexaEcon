dc014e932643dfa991fc77336f3df16dbb9d7587
TheCyanAxolotl <yumahisaiofficial@gmail.com>
2023-05-21 23:58:52 +0200

    HexaEcon

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 <div align="center">
     <a href="https://i.imgur.com/VJkhArh.png">
-        <img src="https://i.imgur.com/xgE01ZK.png" alt="Image description">
+        <img src="https://i.imgur.com/xgE01ZK.png" alt="HexaEcon">
     </a>
     <p align="center">
         <b>Best economy system, replace Vault with HexaEcon you won't be disappointed, HexaEcon offers you various possibilities of using the single mode without activated modules and through the space.kiyoshi.hexaecon.api you can integrate HexaEcon into other plugins and create your own custom economy through the native space.kiyoshi.hexaecon.api, you can use HexaEcon as an economy system for any mode by activating or deactivating the various modules, for versions higher than 1.16 you can also enable NBT support and use a custom texture, custom texture? yes HexaEcon also offers you the possibility of having a physical currency in addition to the virtual one with your own customizable texture, what are you waiting for, try HexaEcon now.</b>
      </p>
   <a href="https://i.imgur.com/iFI45Ej.png">
-        <img src="https://i.imgur.com/iFI45Ej.png" alt="Image description">
+        <img src="https://i.imgur.com/iFI45Ej.png" alt="HexaEcon">
+    </a>
+  <a href="https://i.imgur.com/3oEH0bp.png">
+        <img src="https://i.imgur.com/3oEH0bp.png" alt="HexaEcon">
     </a>
 </div>
---
+++ src/main/java/space/kiyoshi/hexaecon/api/HexaEconAPI.java
@@ -0,0 +1,338 @@
+package space.kiyoshi.hexaecon.api;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import space.kiyoshi.hexaecon.HexaEcon;
+import space.kiyoshi.hexaecon.functions.TableFunctionMongo;
+import space.kiyoshi.hexaecon.functions.TableFunctionRedis;
+import space.kiyoshi.hexaecon.functions.TableFunctionSQL;
+import space.kiyoshi.hexaecon.utils.GetConfig;
+import space.kiyoshi.hexaecon.utils.NMSUtils;
+
+import java.io.File;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+@SuppressWarnings("ALL")
+public class HexaEconAPI {
+    public static void createBankAccount(Player player, Integer value) throws SQLException {
+        String databasetype = GetConfig.INSTANCE.main().getString("DataBase.Type");
+        GetConfig.INSTANCE.generatePlayerConfigAmount(player, value);
+        switch (databasetype) {
+            case "h2" -> {
+                TableFunctionSQL.INSTANCE.createTableAmountSQLite(player, value);
+            }
+            case "MongoDB" -> {
+                TableFunctionMongo.INSTANCE.createCollectionAmount(player.getName(), value);
+            }
+            case "MySQL" -> {
+                TableFunctionSQL.INSTANCE.createTableAmount(player, value);
+            }
+            case "Redis" -> {
+                TableFunctionRedis.INSTANCE.createTableAmount(player.getName(), value);
+            }
+        }
+    }
+
+    public static void deleteBankAccount(Player player) throws SQLException {
+        String databasetype = GetConfig.INSTANCE.main().getString("DataBase.Type");
+        GetConfig.INSTANCE.deletePlayerConfig(player);
+        switch (databasetype) {
+            case "h2" -> {
+                TableFunctionSQL.INSTANCE.dropTableSQLite(player);
+            }
+            case "MongoDB" -> {
+                TableFunctionMongo.INSTANCE.dropCollection(player.getName());
+            }
+            case "MySQL" -> {
+                TableFunctionSQL.INSTANCE.dropTable(player);
+            }
+            case "Redis" -> {
+                TableFunctionRedis.INSTANCE.dropTable(player.getName());
+            }
+        }
+    }
+
+    public static boolean hasBankAccount(Player player) {
+        File playerFolder = new File(HexaEcon.Companion.getPlugin().getDataFolder(), "data/"+player.getName()+"");
+        return playerFolder.exists();
+    }
+
+    public static void setMoney(Player player, Integer value) throws SQLException {
+        String databasetype = GetConfig.INSTANCE.main().getString("DataBase.Type");
+        GetConfig.INSTANCE.deletePlayerConfig(player);
+        GetConfig.INSTANCE.generatePlayerConfigAmount(player, value);
+        switch (databasetype) {
+            case "h2" -> {
+                TableFunctionSQL.INSTANCE.dropTableSQLite(player);
+                TableFunctionSQL.INSTANCE.createTableAmountSQLite(player, value);
+            }
+            case "MongoDB" -> {
+                TableFunctionMongo.INSTANCE.dropCollection(player.getName());
+                TableFunctionMongo.INSTANCE.createCollectionAmount(player.getName(), value);
+            }
+            case "MySQL" -> {
+                TableFunctionSQL.INSTANCE.dropTable(player);
+                TableFunctionSQL.INSTANCE.createTableAmount(player, value);
+            }
+            case "Redis" -> {
+                TableFunctionRedis.INSTANCE.dropTable(player.getName());
+                TableFunctionRedis.INSTANCE.createTableAmount(player.getName(), value);
+            }
+        }
+    }
+
+    public static void addMoney(Player player, Integer value) {
+        String databasetype = GetConfig.INSTANCE.main().getString("DataBase.Type");
+        String dataeconomyvalue = GetConfig.INSTANCE.main().getString("DataBase.DataEconomyName");
+        File data_names2_sqlite =
+                new File(HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName() + "_SQLite.txt");
+        File data_names2_mysql =
+                new File(HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName() + "_MySQL.txt");
+        File data_names2_mongodb =
+                new File(HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName() + "_MongoDB.txt");
+        File data_names2_redis =
+                new File(HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName() + "_Redis.txt");
+        FileConfiguration data_names_config2_sqlite =
+                YamlConfiguration.loadConfiguration(data_names2_sqlite);
+        FileConfiguration data_names_config2_mysql =
+                YamlConfiguration.loadConfiguration(data_names2_mysql);
+        FileConfiguration data_names_config2_mongodb =
+                YamlConfiguration.loadConfiguration(data_names2_mongodb);
+        FileConfiguration data_names_config2_redis =
+                YamlConfiguration.loadConfiguration(data_names2_redis);
+        File data_names_sqlite = new File(
+                HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName()
+                        + "_SQLite.txt"
+        );
+        File data_names_mysql = new File(
+                HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName()
+                        + "_MySQL.txt"
+        );
+        File data_names_mongodb = new File(
+                HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName()
+                        + "_MongoDB.txt"
+        );
+        File data_names_redis = new File(
+                HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName()
+                        + "_Redis.txt"
+        );
+        int somasqlite =
+                data_names_config2_sqlite.getInt("data." + dataeconomyvalue) + value;
+        int somamysql =
+                data_names_config2_mysql.getInt("data." + dataeconomyvalue) + value;
+        int somamongodb =
+                data_names_config2_mongodb.getInt("data." + dataeconomyvalue) + value;
+        int somaredis =
+                data_names_config2_redis.getInt("data." + dataeconomyvalue) + value;
+        try {
+            switch (databasetype) {
+                case "h2" -> {
+                    TableFunctionSQL.INSTANCE.dropTableSQLite(player);
+                }
+                case "MongoDB" -> {
+                    TableFunctionMongo.INSTANCE.dropCollection(player.getName());
+                }
+                case "MySQL" -> {
+                    TableFunctionSQL.INSTANCE.dropTable(player);
+                }
+                case "Redis" -> {
+                    TableFunctionRedis.INSTANCE.dropTable(player.getName());
+                }
+            }
+        } catch (Exception ignored) {}
+        try {
+            switch (databasetype) {
+                case "h2" -> {
+                    TableFunctionSQL.INSTANCE.createTableAmountSQLite(player, somasqlite);
+                }
+                case "MongoDB" -> {
+                    TableFunctionMongo.INSTANCE.createCollectionAmount(player.getName(), somamongodb);
+                }
+                case "MySQL" -> {
+                    TableFunctionSQL.INSTANCE.createTableAmount(player, somamysql);
+                }
+                case "Redis" -> {
+                    TableFunctionRedis.INSTANCE.createTableAmount(player.getName(), somaredis);
+                }
+            }
+        } catch (Exception ignored) {}
+        switch (databasetype) {
+            case "h2" -> {
+                data_names_config2_sqlite.set("data." + dataeconomyvalue, somasqlite);
+            }
+            case "MongoDB" -> {
+                data_names_config2_mongodb.set("data." + dataeconomyvalue, somamongodb);
+            }
+            case "MySQL" -> {
+                data_names_config2_mysql.set("data." + dataeconomyvalue, somamysql);
+            }
+            case "Redis" -> {
+                data_names_config2_redis.set("data." + dataeconomyvalue, somaredis);
+            }
+        }
+        try {
+            switch (databasetype) {
+                case "h2" -> {
+                    data_names_config2_sqlite.save(data_names_sqlite);
+                }
+                case "MongoDB" -> {
+                    data_names_config2_mongodb.save(data_names_mongodb);
+                }
+                case "MySQL" -> {
+                    data_names_config2_mysql.save(data_names_mysql);
+                }
+                case "Redis" -> {
+                    data_names_config2_redis.save(data_names_redis);
+                }
+            }
+        } catch (Exception ignored) {}
+        HexaEcon.Companion.getPlugin().reloadConfig();
+    }
+
+    public static void removeMoney(Player player, Integer value) {
+        String databasetype = GetConfig.INSTANCE.main().getString("DataBase.Type");
+        String dataeconomyvalue = GetConfig.INSTANCE.main().getString("DataBase.DataEconomyName");
+        File data_names_sqlite =
+                new File(HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName() + "_SQLite.txt");
+        File data_names_mysql =
+                new File(HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName() + "_MySQL.txt");
+        File data_names_mongodb =
+                new File(HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName() + "_MongoDB.txt");
+        File data_names_redis =
+                new File(HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName() + "_Redis.txt");
+        FileConfiguration data_names_config_sqlite =
+                YamlConfiguration.loadConfiguration(data_names_sqlite);
+        FileConfiguration data_names_config_mysql =
+                YamlConfiguration.loadConfiguration(data_names_mysql);
+        FileConfiguration data_names_config_mongodb =
+                YamlConfiguration.loadConfiguration(data_names_mongodb);
+        FileConfiguration data_names_config_redis =
+                YamlConfiguration.loadConfiguration(data_names_redis);
+        int somasqlite =
+                data_names_config_sqlite.getInt("data." + dataeconomyvalue) - value;
+        int somamysql =
+                data_names_config_mysql.getInt("data." + dataeconomyvalue) - value;
+        int somamongodb =
+                data_names_config_mongodb.getInt("data." + data_names_config_mongodb) - value;
+        int somaredis =
+                data_names_config_redis.getInt("data." + dataeconomyvalue) - value;
+
+        if (databasetype == "h2") {
+            if (data_names_config_sqlite.getInt("data." + dataeconomyvalue) >= value){
+                try {
+                    switch (databasetype) {
+                        case "h2" ->{
+                            TableFunctionSQL.INSTANCE.dropTableSQLite(player);
+                        }
+                        case "MongoDB" ->{
+                            TableFunctionMongo.INSTANCE.dropCollection(player.getName());
+                        }
+                        case "MySQL" ->{
+                            TableFunctionSQL.INSTANCE.dropTable(player);
+                        }
+                        case "Redis" ->{
+                            TableFunctionRedis.INSTANCE.dropTable(player.getName());
+                        }
+                    }
+                } catch (Exception ignored) {}
+                try {
+                    switch (databasetype) {
+                        case "h2" ->{
+                            TableFunctionSQL.INSTANCE.createTableAmountSQLite(player, somasqlite);
+                        }
+                        case "MongoDB" ->{
+                            TableFunctionMongo.INSTANCE.createCollectionAmount(player.getName(), somamongodb);
+                        }
+                        case "MySQL" ->{
+                            TableFunctionSQL.INSTANCE.createTableAmount(player, somamysql);
+                        }
+                        case "Redis" ->{
+                            TableFunctionRedis.INSTANCE.createTableAmount(player.getName(), somaredis);
+                        }
+                    }
+                } catch (Exception ignored) {}
+                switch (databasetype) {
+                    case "h2" ->{
+                        data_names_config_sqlite.set("data." + dataeconomyvalue, somasqlite);
+                    }
+                    case "MongoDB" ->{
+                        data_names_config_mongodb.set("data." + dataeconomyvalue, somamongodb);
+                    }
+                    case "MySQL" ->{
+                        data_names_config_mysql.set("data." + dataeconomyvalue, somamysql);
+                    }
+                    case "Redis" ->{
+                        data_names_config_redis.set("data." + dataeconomyvalue, somaredis);
+                    }
+                }
+                try {
+                    switch (databasetype) {
+                        case "h2" ->{
+                            data_names_config_sqlite.save(data_names_sqlite);
+                        }
+                        case "MongoDB" ->{
+                            data_names_config_mongodb.save(data_names_mongodb);
+                        }
+                        case "MySQL" ->{
+                            data_names_config_mysql.save(data_names_mysql);
+                        }
+                        case "Redis" ->{
+                            data_names_config_redis.save(data_names_redis);
+                        }
+                    }
+                } catch (Exception ignored) {}
+                HexaEcon.Companion.getPlugin().reloadConfig();
+            }
+        }
+    }
+
+    public static String getPlayerBalance(Player player) throws SQLException {
+        String databasetype = GetConfig.INSTANCE.main().getString("DataBase.Type");
+        String dataeconomyvalue = GetConfig.INSTANCE.main().getString("DataBase.DataEconomyName");
+        switch (databasetype) {
+            case "h2" -> {
+                return TableFunctionSQL.selectAllFromTableAsStringSQLite(player.getName()).toString().replace("[", "").replace("]", "");
+            }
+            case "MongoDB" -> {
+                return TableFunctionMongo.INSTANCE.selectAllFromCollectionAsString(player.getName()).toString().replace("[", "").replace("]", "");
+            }
+            case "MySQL" -> {
+                Statement stmt = HexaEcon.Companion.getMySQLManager().getConnection().createStatement();
+                String SQL = "SELECT * FROM " + player.getName();
+                ResultSet rs = stmt.executeQuery(SQL);
+                rs.next();
+                String value = String.valueOf(rs.getInt(dataeconomyvalue));
+                rs.close();
+                stmt.close();
+                return value;
+            }
+            case "Redis" -> {
+                TableFunctionRedis.selectAllFromCollectionAsStringRedis(player.getName()).toString().replace("[", "").replace("]", "");
+            }
+        }
+        return "Unknown Balance";
+    }
+
+    public static boolean hasPlayerEnoughMoney(Player player, int amount) throws SQLException {
+        String playerBalance = getPlayerBalance(player);
+        if (playerBalance != null) {
+            try {
+                int balance = Integer.parseInt(playerBalance);
+                return balance >= amount;
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public static boolean canUseTexturePack() {
+        return NMSUtils.INSTANCE.checkServerVersionUp(NMSUtils.INSTANCE.getCleanServerVersion());
+    }
+
+
+
+}
