819ac545efb4344de9915a06e5add5e878163fc9
TheCyanAxolotl <yumahisaiofficial@gmail.com>
2023-05-24 19:58:14 +0200

    HexaEcon

--- src/main/java/space/kiyoshi/hexaecon/api/HexaEconAPI.java
+++ src/main/java/space/kiyoshi/hexaecon/api/HexaEconAPI.java
@@ -94,207 +94,84 @@ public class HexaEconAPI {
     public static void addMoney(Player player, Long value) {
         String databasetype = GetConfig.INSTANCE.main().getString("DataBase.Type");
         String dataeconomyvalue = GetConfig.INSTANCE.main().getString("DataBase.DataEconomyName");
-        File data_names2_sqlite =
-                new File(HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName() + "_SQLite.txt");
-        File data_names2_mysql =
-                new File(HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName() + "_MySQL.txt");
-        File data_names2_mongodb =
-                new File(HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName() + "_MongoDB.txt");
-        File data_names2_redis =
-                new File(HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName() + "_Redis.txt");
-        FileConfiguration data_names_config2_sqlite =
-                YamlConfiguration.loadConfiguration(data_names2_sqlite);
-        FileConfiguration data_names_config2_mysql =
-                YamlConfiguration.loadConfiguration(data_names2_mysql);
-        FileConfiguration data_names_config2_mongodb =
-                YamlConfiguration.loadConfiguration(data_names2_mongodb);
-        FileConfiguration data_names_config2_redis =
-                YamlConfiguration.loadConfiguration(data_names2_redis);
-        File data_names_sqlite = new File(
-                HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName()
-                        + "_SQLite.txt"
-        );
-        File data_names_mysql = new File(
-                HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName()
-                        + "_MySQL.txt"
-        );
-        File data_names_mongodb = new File(
-                HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName()
-                        + "_MongoDB.txt"
-        );
-        File data_names_redis = new File(
-                HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName()
-                        + "_Redis.txt"
-        );
-        long somasqlite =
-                data_names_config2_sqlite.getLong("data." + dataeconomyvalue) + value;
-        long somamysql =
-                data_names_config2_mysql.getLong("data." + dataeconomyvalue) + value;
-        long somamongodb =
-                data_names_config2_mongodb.getLong("data." + dataeconomyvalue) + value;
-        long somaredis =
-                data_names_config2_redis.getLong("data." + dataeconomyvalue) + value;
+        File dataFolder = HexaEcon.Companion.getPlugin().getDataFolder();
+        File dataFile = new File(dataFolder, "/data/" + player.getName() + "/" + player.getName() + "_" + databasetype + ".txt");
+        FileConfiguration dataConfig = YamlConfiguration.loadConfiguration(dataFile);
+
+        long currentAmount = dataConfig.getLong("data." + dataeconomyvalue, 0);
+        long newAmount = currentAmount + value;
+
         try {
             switch (databasetype) {
-                case "h2" -> {
+                case "h2":
                     TableFunctionSQL.INSTANCE.dropTableSQLite(player);
-                }
-                case "MongoDB" -> {
+                    TableFunctionSQL.INSTANCE.createTableAmountSQLite(player, newAmount);
+                    break;
+                case "MongoDB":
                     TableFunctionMongo.INSTANCE.dropCollection(player.getName());
-                }
-                case "MySQL" -> {
+                    TableFunctionMongo.INSTANCE.createCollectionAmount(player.getName(), newAmount);
+                    break;
+                case "MySQL":
                     TableFunctionSQL.INSTANCE.dropTable(player);
-                }
-                case "Redis" -> {
+                    TableFunctionSQL.INSTANCE.createTableAmount(player, newAmount);
+                    break;
+                case "Redis":
                     TableFunctionRedis.INSTANCE.dropTable(player.getName());
-                }
+                    TableFunctionRedis.INSTANCE.createTableAmount(player.getName(), newAmount);
+                    break;
             }
-        } catch (Exception ignored) {}
-        try {
-            switch (databasetype) {
-                case "h2" -> {
-                    TableFunctionSQL.INSTANCE.createTableAmountSQLite(player, somasqlite);
-                }
-                case "MongoDB" -> {
-                    TableFunctionMongo.INSTANCE.createCollectionAmount(player.getName(), somamongodb);
-                }
-                case "MySQL" -> {
-                    TableFunctionSQL.INSTANCE.createTableAmount(player, somamysql);
-                }
-                case "Redis" -> {
-                    TableFunctionRedis.INSTANCE.createTableAmount(player.getName(), somaredis);
-                }
-            }
-        } catch (Exception ignored) {}
-        switch (databasetype) {
-            case "h2" -> {
-                data_names_config2_sqlite.set("data." + dataeconomyvalue, somasqlite);
-            }
-            case "MongoDB" -> {
-                data_names_config2_mongodb.set("data." + dataeconomyvalue, somamongodb);
-            }
-            case "MySQL" -> {
-                data_names_config2_mysql.set("data." + dataeconomyvalue, somamysql);
-            }
-            case "Redis" -> {
-                data_names_config2_redis.set("data." + dataeconomyvalue, somaredis);
-            }
+
+            dataConfig.set("data." + dataeconomyvalue, newAmount);
+            dataConfig.save(dataFile);
+
+            HexaEcon.Companion.getPlugin().reloadConfig();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        try {
-            switch (databasetype) {
-                case "h2" -> {
-                    data_names_config2_sqlite.save(data_names_sqlite);
-                }
-                case "MongoDB" -> {
-                    data_names_config2_mongodb.save(data_names_mongodb);
-                }
-                case "MySQL" -> {
-                    data_names_config2_mysql.save(data_names_mysql);
-                }
-                case "Redis" -> {
-                    data_names_config2_redis.save(data_names_redis);
-                }
-            }
-        } catch (Exception ignored) {}
-        HexaEcon.Companion.getPlugin().reloadConfig();
     }
+
 
     public static void removeMoney(Player player, Long value) {
         String databasetype = GetConfig.INSTANCE.main().getString("DataBase.Type");
         String dataeconomyvalue = GetConfig.INSTANCE.main().getString("DataBase.DataEconomyName");
-        File data_names_sqlite =
-                new File(HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName() + "_SQLite.txt");
-        File data_names_mysql =
-                new File(HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName() + "_MySQL.txt");
-        File data_names_mongodb =
-                new File(HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName() + "_MongoDB.txt");
-        File data_names_redis =
-                new File(HexaEcon.Companion.getPlugin().getDataFolder().toString() + "/data/"+player.getName()+"/" + player.getName() + "_Redis.txt");
-        FileConfiguration data_names_config_sqlite =
-                YamlConfiguration.loadConfiguration(data_names_sqlite);
-        FileConfiguration data_names_config_mysql =
-                YamlConfiguration.loadConfiguration(data_names_mysql);
-        FileConfiguration data_names_config_mongodb =
-                YamlConfiguration.loadConfiguration(data_names_mongodb);
-        FileConfiguration data_names_config_redis =
-                YamlConfiguration.loadConfiguration(data_names_redis);
-        long somasqlite =
-                data_names_config_sqlite.getLong("data." + dataeconomyvalue) - value;
-        long somamysql =
-                data_names_config_mysql.getLong("data." + dataeconomyvalue) - value;
-        long somamongodb =
-                data_names_config_mongodb.getLong("data." + data_names_config_mongodb) - value;
-        long somaredis =
-                data_names_config_redis.getLong("data." + dataeconomyvalue) - value;
+        File dataFolder = HexaEcon.Companion.getPlugin().getDataFolder();
+        File dataFile = new File(dataFolder, "/data/" + player.getName() + "/" + player.getName() + "_" + databasetype + ".txt");
+        FileConfiguration dataConfig = YamlConfiguration.loadConfiguration(dataFile);
 
-        if (databasetype == "h2") {
-            if (data_names_config_sqlite.getLong("data." + dataeconomyvalue) >= value){
-                try {
-                    switch (databasetype) {
-                        case "h2" ->{
-                            TableFunctionSQL.INSTANCE.dropTableSQLite(player);
-                        }
-                        case "MongoDB" ->{
-                            TableFunctionMongo.INSTANCE.dropCollection(player.getName());
-                        }
-                        case "MySQL" ->{
-                            TableFunctionSQL.INSTANCE.dropTable(player);
-                        }
-                        case "Redis" ->{
-                            TableFunctionRedis.INSTANCE.dropTable(player.getName());
-                        }
-                    }
-                } catch (Exception ignored) {}
-                try {
-                    switch (databasetype) {
-                        case "h2" ->{
-                            TableFunctionSQL.INSTANCE.createTableAmountSQLite(player, somasqlite);
-                        }
-                        case "MongoDB" ->{
-                            TableFunctionMongo.INSTANCE.createCollectionAmount(player.getName(), somamongodb);
-                        }
-                        case "MySQL" ->{
-                            TableFunctionSQL.INSTANCE.createTableAmount(player, somamysql);
-                        }
-                        case "Redis" ->{
-                            TableFunctionRedis.INSTANCE.createTableAmount(player.getName(), somaredis);
-                        }
-                    }
-                } catch (Exception ignored) {}
+        long currentAmount = dataConfig.getLong("data." + dataeconomyvalue, 0);
+        long newAmount = currentAmount - value;
+
+        if (newAmount >= 0) {
+            try {
                 switch (databasetype) {
-                    case "h2" ->{
-                        data_names_config_sqlite.set("data." + dataeconomyvalue, somasqlite);
-                    }
-                    case "MongoDB" ->{
-                        data_names_config_mongodb.set("data." + dataeconomyvalue, somamongodb);
-                    }
-                    case "MySQL" ->{
-                        data_names_config_mysql.set("data." + dataeconomyvalue, somamysql);
-                    }
-                    case "Redis" ->{
-                        data_names_config_redis.set("data." + dataeconomyvalue, somaredis);
-                    }
+                    case "h2":
+                        TableFunctionSQL.INSTANCE.dropTableSQLite(player);
+                        TableFunctionSQL.INSTANCE.createTableAmountSQLite(player, newAmount);
+                        break;
+                    case "MongoDB":
+                        TableFunctionMongo.INSTANCE.dropCollection(player.getName());
+                        TableFunctionMongo.INSTANCE.createCollectionAmount(player.getName(), newAmount);
+                        break;
+                    case "MySQL":
+                        TableFunctionSQL.INSTANCE.dropTable(player);
+                        TableFunctionSQL.INSTANCE.createTableAmount(player, newAmount);
+                        break;
+                    case "Redis":
+                        TableFunctionRedis.INSTANCE.dropTable(player.getName());
+                        TableFunctionRedis.INSTANCE.createTableAmount(player.getName(), newAmount);
+                        break;
                 }
-                try {
-                    switch (databasetype) {
-                        case "h2" ->{
-                            data_names_config_sqlite.save(data_names_sqlite);
-                        }
-                        case "MongoDB" ->{
-                            data_names_config_mongodb.save(data_names_mongodb);
-                        }
-                        case "MySQL" ->{
-                            data_names_config_mysql.save(data_names_mysql);
-                        }
-                        case "Redis" ->{
-                            data_names_config_redis.save(data_names_redis);
-                        }
-                    }
-                } catch (Exception ignored) {}
+
+                dataConfig.set("data." + dataeconomyvalue, newAmount);
+                dataConfig.save(dataFile);
+
                 HexaEcon.Companion.getPlugin().reloadConfig();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
+
 
     public static String getPlayerBalance(Player player) throws SQLException {
         String databasetype = GetConfig.INSTANCE.main().getString("DataBase.Type");
